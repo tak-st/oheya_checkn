d40a9b2d02a174dcc2cace6f24dcc96e40655b65
Akisu <akisu1016@yahoo.co.jp>
2020-12-04 16:31:00 +0900

    データクラスにcli表示とDB処理を追加しました

--- device/measure_data.py
+++ device/measure_data.py
@@ -5,22 +5,22 @@ import pymysql
 
 class MeasureClass:
 
-  def __init__(self, data):
+  def __init__(self, data, device_id):
     self.measure_data = data
+    self.device_id = device_id
 
   def data_display(self):
     lcd = jlcd.Jlcd(2,0x27,True)
 
-    if type(self.measure_data) is dict :
-      count = 1
-
-      for measure_data_value in measure_data.values():
-        lcd.message(str(self.measure_data), count)
-        count += 1
-
-    else:
-      lcd.message(str(self.measure_data), 1)
-      lcd.message(str(self.measure_data), 2)
+    if "temp" in self.measure_data:
+      lcd.message("キオン : " + str(self.measure_data["temp"]), 1)
+      lcd.message("シツド : " + str(self.measure_data["humidity"]), 2)
+    elif "co2" in self.measure_data:
+      lcd.message("CO2 : " + str(self.measure_data["co2"]), 1)
+      cd.message("", 2)
+    elif "latitude" in self.measure_data:
+      lcd.message("イド : " + str(self.measure_data["latitude"]), 1)
+      lcd.message("ケイド : " + str(self.measure_data["longitude"]), 2)
 
   def data_post_db(self):
     pymysql.install_as_MySQLdb()
@@ -28,14 +28,25 @@ class MeasureClass:
     cursor = connection.cursor()
     time = datetime.datetime.now()
 
-    try:
-      if type(self.measure_data) is dict:
-        print("post" + self.measure_data)
-      else:
-        print("post" + self.measure_data)
+    if "temp" in self.measure_data:
+      cur.execute("INSERT INTO device_data VALUES('" + str(1) + "','" + "temperature"
+                  + "','" + str(time) + "','" + str(self.measure_data["temp"]) + "')")
+      cur.execute("INSERT INTO device_data VALUES('" + str(1) + "','" + "humidity"
+                  + "','" + str(time) + "','" + str(self.measure_data["humidity"]) + "')")
+    elif "co2" in self.measure_data:
+      cur.execute("INSERT INTO device_data VALUES('" + str(1) + "','" + "co2"
+                  + "','" + str(time) + "','" + str(self.measure_data["co2"]) + "')")
+    elif "latitude" in self.measure_data:
+      cur.execute("INSERT INTO device_data VALUES('" + str(1) + "','" + "latitude"
+                  + "','" + str(time) + "','" + str(self.measure_data["latitude"]) + "')")
+      cur.execute("INSERT INTO device_data VALUES('" + str(1) + "','" + "longitude"
+                  + "','" + str(time) + "','" + str(self.measure_data["longitude"]) + "')")
 
     except MySQLdb.Error as e:
       print(e)
 
     connection.commit()
     connection.close()
+
+  def data_print(self):
+    print(str(self.measure_data))
