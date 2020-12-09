b5564e6a29bf9c36920f3e3d0193d9952bff9e2c
Akisu <akisu1016@yahoo.co.jp>
2020-12-09 14:14:06 +0900

    内部データベースの初期設定を追加

--- device/setup.py
+++ device/setup.py
@@ -1,6 +1,6 @@
 #coding:utf-8
 from uuid import getnode as get_mac
-from lcddisplay import jlcd
+#from lcddisplay import jlcd
 import random
 import math
 import sqlite3
@@ -10,7 +10,7 @@ class first_setup:
     def __init__(self):
         #MACアドレスをランダムなシード値で再作成
         random.seed(get_mac())
-        mac = math.floor(random.random()*1000000)
+        self.mac = math.floor(random.random()*1000000)
 
     def id_display(self):
         #LCDに表示
@@ -20,20 +20,21 @@ class first_setup:
     def create_database(self):
         DATABASE = 'my_air_data.db'
         conn = sqlite3.connect(DATABASE)
+
         device_sql = '''
                 CREATE TABLE IF NOT EXISTS device(
                 device_id VARCHAR NOT NULL,
-                device_name VARCHAR NOT NULL,
+                device_name VARCHAR,
                 PRIMARY KEY (device_id))
             '''
         sensor_sql = '''
-                CREATE TABLE IF NOT EXISTS senosr(
+                CREATE TABLE IF NOT EXISTS sensor(
                 sensor_id INTEGER NOT NULL,
                 sensor_name VARCHAR,
                 sensor_type VARCHAR,
                 sensor_unit VARCHAR,
-                sensor_min INTEGER,
-                sensor_max INTEGER,
+                sensor_min REAL,
+                sensor_max REAL,
                 PRIMARY KEY (sensor_id))
             '''
         device_data_sql = '''
@@ -50,5 +51,46 @@ class first_setup:
         conn.execute(sensor_sql)
         conn.execute(device_data_sql)
 
+        #device表にアドレスを挿入
+        conn.execute(f'INSERT INTO device(device_id, device_name) values("{self.mac}", null)')
+
+        #sensor表にデータを挿入
+        conn.execute('''INSERT INTO sensor(
+            sensor_id, sensor_name, sensor_type, sensor_unit, sensor_min, sensor_max)
+            values(1, "DHT22", "Temperature", "°C", -50, 120)'''
+            )
+
+        conn.execute('''INSERT INTO sensor(
+            sensor_id, sensor_name, sensor_type, sensor_unit, sensor_min, sensor_max)
+            values(2, "DHT22", "Humid", "%", 0, 100)'''
+            )
+        conn.execute('''INSERT INTO sensor(
+            sensor_id, sensor_name, sensor_type, sensor_unit, sensor_min, sensor_max)
+            values(3, "GYSFDMAXB", "Latitude", "", 0, 360)'''
+            )
+
+        conn.execute('''INSERT INTO sensor(
+            sensor_id, sensor_name, sensor_type, sensor_unit, sensor_min, sensor_max)
+            values(4, "GYSFDMAXB", "Longitude", "", 0, 360)'''
+            )
+
+        conn.execute('''INSERT INTO sensor(
+            sensor_id, sensor_name, sensor_type, sensor_unit, sensor_min, sensor_max)
+            values(5, "MH-Z19B", "CO2", "ppm", 0, 9999)'''
+            )
+
+        conn.execute('''INSERT INTO sensor(
+            sensor_id, sensor_name, sensor_type, sensor_unit, sensor_min, sensor_max)
+            values(6, "GP2Y1026AU0F", "Dust", "mg/m³", 0, 9999)'''
+            )
+
+        conn.execute('''INSERT INTO sensor(
+            sensor_id, sensor_name, sensor_type, sensor_unit, sensor_min, sensor_max)
+            values(7, "MQ-135", "Gas", "", 0, 1000)'''
+            )
+
         conn.commit()
         conn.close()
+
+Setup = first_setup()
+Setup.create_database()
