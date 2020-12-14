751d268ea7a0a41cb0cf6fd3c6e1e61aa0d6cda0
Akisu <akisu1016@yahoo.co.jp>
2020-12-14 15:39:30 +0900

    最新データの差分比較用のMAXtimeを取得する処理を追加

--- device/operate_database.py
+++ device/operate_database.py
@@ -33,13 +33,26 @@ class OperateLocalDatabase:
     finally:
       connection.close()
 
+  def select_maxtime(self, device_id):
+    connection = sqlite3.connect(self.DATABASE)
+    try:
+      with connection.cursor() as cursor:
+        cursor.execute("SELECT MAX(time) FROM device_data WHERE %s GROUP BY device_id", device_id)
+        maxtime = cursor.fetchone()
+
+        return maxtime
+    except sqlite3.Error as e:
+      print(e)
+    finally:
+      connection.close()
+
 class OperateRemoteDatabase:
   def __init__(self):
     self.connection = db.connect_air_database()
 
   def select_data(self, sensor_id):
     try:
-      with connection.cursor() as cursor:
+      with self.connection.cursor() as cursor:
         cursor.execute("SELECT * FROM device_data WHERE %s", sensor_id)
         device_data = cursor.fetchall()
 
@@ -51,9 +64,21 @@ class OperateRemoteDatabase:
 
   def insert_data(self, device_id, sensor_id, sensor_data, time):
     try:
-      with connection.cursor() as cursor:
+      with self.connection.cursor() as cursor:
         cursor.execute("INSERT INTO device_data VALUES(%s, %d, %s, %s)", device_id, sensor_id, sensor_data, time)
         conn.commit()
+    except pymysql.Error as e:
+      print(e)
+    finally:
+      connection.close()
+
+    def select_maxtime(self, device_id):
+    try:
+      with self.connection.cursor() as cursor:
+        cursor.execute("SELECT MAX(time) FROM device_data WHERE %s GROUP BY device_id", device_id)
+        maxtime = cursor.fetchone()
+
+        return maxtime
     except pymysql.Error as e:
       print(e)
     finally:
